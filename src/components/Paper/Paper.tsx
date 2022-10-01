e4a8308346dbd3c1fa322a3a415a0d1f9747e6bd
tocomi <tocomi0112@gmail.com>
2022-10-01 12:20:31 +0900

    feat: change paper elevation

--- src/components/Paper/Paper.tsx
+++ src/components/Paper/Paper.tsx
@@ -2,5 +2,5 @@ import { Paper as MuiPaper } from '@mui/material'
 import { ComponentProps, FC, memo } from 'react'
 
 export const Paper: FC<ComponentProps<typeof MuiPaper>> = memo(function Paper(props) {
-  return <MuiPaper elevation={4} {...props} />
+  return <MuiPaper elevation={8} {...props} />
 })
