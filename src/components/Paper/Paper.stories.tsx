ea3bfd5f49232116cbb362b900ce575829470b35
tocomi <tocomi0112@gmail.com>
2022-10-01 12:15:10 +0900

    feat: add paper component

---
+++ src/components/Paper/Paper.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { Paper } from './Paper'
+
+export default { component: Paper } as ComponentMeta<typeof Paper>
+
+export const Index: ComponentStoryObj<typeof Paper> = {
+  args: {
+    sx: (theme) => ({
+      bgcolor: theme.palette.primary.main,
+      height: 200,
+      width: 300,
+    }),
+  },
+}
---
+++ src/components/Paper/Paper.tsx
@@ -0,0 +1,6 @@
+import { Paper as MuiPaper } from '@mui/material'
+import { ComponentProps, FC, memo } from 'react'
+
+export const Paper: FC<ComponentProps<typeof MuiPaper>> = memo(function Paper(props) {
+  return <MuiPaper elevation={4} {...props} />
+})
